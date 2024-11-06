69ad25afab0496aaa03f9d7eb32ba6ab5370986e
udontur <h@hadrianlau.com>
2024-11-06 22:05:19 +0800

    Use english

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Moving the program to /usr/bin"
+echo "Moving the program to /usr/bin..."
 sudo cp ~/ossd-mark-cal/apps/ossd-mark-cal.out /usr/bin/ossd-mark-cal.out
-echo "Creating the ossdmk command in /usr/local/bin/ossdmk"
+echo "Creating the ossdmk command in /usr/local/bin/ossdmk..."
 sudo cp ~/ossd-mark-cal/apps/ossdmk /usr/local/bin/ossdmk
-echo "Cleaning up the github repository"
+echo "Cleaning up the github repository."
 rm -Rf ~/ossd-mark-cal 
-echo "Installation Complete! Please type ossdmk to get started"
+echo "Installation Complete! Please type ossdmk to get started."
