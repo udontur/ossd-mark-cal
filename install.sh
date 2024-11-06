1a6c9c5ee49a22f3f804db36c8df671280ad838b
udontur <h@hadrianlau.com>
2024-11-06 22:14:22 +0800

    Fixing the bug fix

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ sudo cp ~/ossd-mark-cal/apps/ossd-mark-cal.out /usr/bin/ossd-mark-cal.out
 echo "Creating the ossdmk command in /usr/local/bin/ossdmk..."
 sudo cp ~/ossd-mark-cal/apps/ossdmk /usr/local/bin/ossdmk
 echo "Cleaning up the environment."
-rm -Rf ~/ossd-mark-cal 
 cd ~
+rm -Rf ~/ossd-mark-cal 
 echo "Installation Complete! Please type ossdmk to get started."
