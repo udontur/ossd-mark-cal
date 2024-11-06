943cff09ee27d95c4960c994c79edc685aa8e51e
udontur <h@hadrianlau.com>
2024-11-06 22:10:58 +0800

    Fix the "no parent directory" bug

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ echo "Moving the program to /usr/bin..."
 sudo cp ~/ossd-mark-cal/apps/ossd-mark-cal.out /usr/bin/ossd-mark-cal.out
 echo "Creating the ossdmk command in /usr/local/bin/ossdmk..."
 sudo cp ~/ossd-mark-cal/apps/ossdmk /usr/local/bin/ossdmk
-echo "Cleaning up the github repository."
+echo "Cleaning up the environment."
 rm -Rf ~/ossd-mark-cal 
+cd ~
 echo "Installation Complete! Please type ossdmk to get started."
