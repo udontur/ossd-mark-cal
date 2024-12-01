4c5700e785aaede35491c38b4e51f7176ff63610
udontur <h@hadrianlau.com>
2024-12-01 20:34:36 +0800

    Update code format

--- src/src.cpp
+++ src/src.cpp
@@ -1,13 +1,6 @@
-/*******************************************************
-* Location: /usr/bin/ossd-mark-cal.out                 *
-* GitHub link: github.com/udontur/ossd-mark-calculator *
-* Date: July 2024                                      *
-* Author: github.com/udontur                           *
-* OS: Linux only                                       *
-*******************************************************/
-
 #include <iomanip>
 #include <iostream>
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
@@ -22,7 +15,6 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < numberOfItems; i++) {
         double markInput;
         cin >> markInput;
-        // Multiply the decimals to integers to maximize accuracy (3 decimal points)
         mark[i] = static_cast<long long>(markInput * 1000000);
     }
 
@@ -31,7 +23,6 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < numberOfItems; i++) {
         long long denominatorInput;
         cin >> denominatorInput;
-        // Calculate the mark with the denominator
         mark[i] /= denominatorInput;
     }
 
@@ -41,9 +32,7 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < numberOfItems; i++) {
         double weightInput;
         cin >> weightInput;
-        // store the weighting information for calculating the weighting
         totalWeight += weightInput * 10;
-        // Multiply the weight to match the previous decimal scale up
         weight[i] = weightInput * 10000000;
     }
 
@@ -51,31 +40,27 @@ int main(int argc, char* argv[]) {
 
     long long exactMarkProcess = 0;
     for (int i = 0; i < numberOfItems; i++) {
-        // Calculate the mark with the weighting
         long long sectionMark = mark[i] * (weight[i] / totalWeight);
-        // Decimal scale down after the calculation (keep 3+1 scaled up decimal points)
         exactMarkProcess += sectionMark / 1000000;
     }
-    // Round off the exact mark
-    if (exactMarkProcess % 10 >= 5) {
+
+    if (exactMarkProcess % 10 >= 5)
         exactMarkProcess += 10;
-    }
-    // Remove the extra scaled up decimal point
     exactMarkProcess /= 10;
 
-    // Round off and scale down the final mark (no decimals) extracted from exact mark
     int finalMark = exactMarkProcess;
     finalMark /= 100;
-    if (finalMark % 10 >= 5) {
+    if (finalMark % 10 >= 5)
         finalMark += 10;
-    }
     finalMark /= 10;
 
-    // Scale down the exact mark
     double exactMark = static_cast<double>(exactMarkProcess);
     exactMark /= 1000.0;
 
     cout << "\033[92mFinal Mark: " << finalMark << "\033[39m\n";
     cout << fixed << setprecision(3);
     cout << "\033[37mExact Mark: " << exactMark << "\033[39m\n";
+
+    return 0;
+    
 }
